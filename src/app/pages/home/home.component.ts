6d8bc063f14eee1614ff94f9ee72b216e2463483
Chetna Choudhury <chetnachoudhury0804@gmail.com>
2023-06-02 17:41:10 +0530

    array in javascript

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,4 +9,23 @@ export class HomeComponent {
  public data="hello World!!";
  public color= "text-primary";
  public data2 = false;
+ public arr1=[
+ {  name: "aaaaaa",
+    id:1,
+},{
+  name:"bbbbb",
+  id:2
+},
+{
+  name:"cccc",
+  id:3
+},
+{
+  name:"ddddd",
+  id:4
+},
+{
+  name:"eeee",
+  id:5
 }
+ ]}
